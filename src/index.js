bedd6eaef360bd4a2dc4cef9f882b5e44b11bc8b
Serhii-Ter <serhij86@gmail.com>
2022-07-16 00:59:18 +0300

    added buttons in slider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@
 
 new Swiper('.rewiews-slider', {
   navigation: {
-    nextEl: '',
-    prevEl: '',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUp: true,
+  },
+
+  autoHeight: true,
 });
