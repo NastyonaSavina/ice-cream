5a4678599ea88668027166207c1eeb35cef8433d
Serhii-Ter <serhij86@gmail.com>
2022-07-16 00:09:57 +0300

    added styles for rewiew section

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Swiper initialisation
+
+new Swiper('.rewiews-slider', {
+  navigation: {
+    nextEl: '',
+    prevEl: '',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
