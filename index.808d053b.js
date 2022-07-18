9b3402c7ddf1cbb729d477d5e1cae2a30f8ebb86
Nastyona22 <103425739+Nastyona22@users.noreply.github.com>
2022-07-18 17:33:50 +0000

    Deploying to gh-pages from @ Nastyona22/project-group-27@07985a2c057900011cb862e11172f96857bd9bb4 🚀

--- index.808d053b.js
+++ index.808d053b.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.d59ee874.js.map
+//# sourceMappingURL=index.808d053b.js.map
