656f62f9922ff95ba5b94199b317aabd0df7fd77
Nastyona22 <103425739+Nastyona22@users.noreply.github.com>
2022-07-18 14:21:49 +0000

    Deploying to gh-pages from @ Nastyona22/project-group-27@f58fb9f67dbc4ce23b6d3ee95b383b1a91ccc453 🚀

---
+++ index.16fb7b9b.js
@@ -0,0 +1,2 @@
+new Swiper(".rewiews-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},spaceBetween:50,speed:1e3,autoheight:!0,simulateTouch:!0,touchAngle:45,grabCursor:!0});
+//# sourceMappingURL=index.16fb7b9b.js.map
